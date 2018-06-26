dd7ec7784b16f269e2e8b4410e851f0e3bded94f
Marcin Włoczko <mwloczko@gmail.com>
2018-06-26 12:13:41 +0200

    finish

--- Objective-C basics/Objective-C basics/main.m
+++ Objective-C basics/Objective-C basics/main.m
@@ -8,9 +8,30 @@
 
 #import <Foundation/Foundation.h>
 #import "Animal.h"
+#import "Cat.h"
+#import "Circus.h"
+#import "Ant.h"
+#import "Zoo.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        Animal *whiskas = [[Cat alloc] initWithName:@"Whiskas"];
+        Animal *puszek = [[Cat alloc] initWithName:@"Puszek"];
+        Animal *ant = [[Ant alloc] initWithName:@"Ant"];
+        Circus *circus = [[Circus alloc] initWithName:@"Daydream"];
+        
+        [circus.animals addObject:puszek];
+        [circus.animals addObject:ant];
+        [circus.animals addObject:whiskas];
+        
+        NSString *sounds = [circus animalsIntroduction];
+        NSLog(@"animals Intrduction: %@", sounds);
+        
+        NSString *tricks = [circus show];
+        NSLog(@"Tricks: %@ ", tricks);
+        
+        NSInteger patter = [circus patter:4];
+        NSLog(@"Patter: %ld", (long)patter);
         
     }
     return 0;
