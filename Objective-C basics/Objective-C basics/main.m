6e3c6529343629f8e9e810fb71b59d337d0f178f
Marcin Włoczko <mwloczko@gmail.com>
2018-06-26 10:27:07 +0200

    init

---
+++ Objective-C basics/Objective-C basics/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-C basics
+//
+//  Created by Marcin Włoczko on 26.06.2018.
+//  Copyright © 2018 Marcin Włoczko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
